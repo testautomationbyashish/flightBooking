c82c9bf0f97fb3dd7e87b462b7aa00b121afea9a
testautomationbyashish <85038148+testautomationbyashish@users.noreply.github.com>
2021-06-04 12:48:03 +0530

    Update FlightSearchResultsPageActions.java

--- src/main/java/com/blazedemo/pages/FlightSearchResultsPageActions.java
+++ src/main/java/com/blazedemo/pages/FlightSearchResultsPageActions.java
@@ -39,37 +39,15 @@ public class FlightSearchResultsPageActions {
 		
 	}
 	
-	public void clickToChooseDeparture_ReturnFlights(WebElement departList,WebElement returnList,int depart,int retu)
+	public void clickToChooseDeparture_ReturnFlights()
 	{
-		Reporter.addStepLog("Selecting " + depart +"Flight");
-		WebElement departelement=departList.findElement(By.xpath("li["+depart+"]"));
-		List<WebElement> colData = departelement.findElements(By.tagName("th"));
-		colData.get(depart).click();
-		Reporter.addStepLog("Selecting " + retu +"Flight");
-		WebElement Returnelement=returnList.findElement(By.xpath("li["+retu+"]"));
-		List<WebElement> retcolData = Returnelement.findElements(By.tagName("th"));
-		retcolData.get(retu).click();
+		
         
 	}
 	
 	
 	
-	public java.util.List<String> getSelectedRowData(WebElement listview ,int row) throws Throwable {
-        
-		
-	WebElement element=listview.findElement(By.xpath("li["+row+"]"));
-        java.util.List<String> flightData = new ArrayList<String>();
-        List<WebElement> colData = element.findElements(By.tagName("th"));
-        System.out.println("table column size:" + colData.size());
-        Reporter.addStepLog("Flight Details");
-        flightData.add(colData.get(1).findElement(By.cssSelector("img")).getAttribute("title"));
-        Reporter.addStepLog(colData.get(1).getAttribute("class")+":" + colData.get(1).findElement(By.cssSelector("img")).getAttribute("title"));
-        for (int i = 2; i < colData.size(); i++) {
-            String str = colData.get(i).getText();
-            Reporter.addStepLog(colData.get(i).getAttribute("class")+":" + str);
-            System.out.println(colData.get(i).getAttribute("class")+":" + str);
-            flightData.add(str);
-        }
+	
 
         return flightData;
 
