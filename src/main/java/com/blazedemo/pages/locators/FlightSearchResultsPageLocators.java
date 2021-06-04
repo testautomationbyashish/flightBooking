984a135941aac14570f9e820446343f9f5fcec91
testautomationbyashish <85038148+testautomationbyashish@users.noreply.github.com>
2021-06-04 12:50:46 +0530

    Update FlightSearchResultsPageLocators.java

--- src/main/java/com/blazedemo/pages/locators/FlightSearchResultsPageLocators.java
+++ src/main/java/com/blazedemo/pages/locators/FlightSearchResultsPageLocators.java
@@ -8,20 +8,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
-import com.clearTrip.utils.SeleniumDriver;
+import com.blazedemo.utils.SeleniumDriver;
 
 public class FlightSearchResultsPageLocators {
 	
-	@FindBy(xpath="//div[@class='colZero leg col12']//ul[@class='listView flights']")
-	public WebElement SearchResults_FromFlightsListView;
-	
-	@FindBy(xpath="//div[@class='colZero leg col12 last']//ul[@class='listView flights']")
-	public WebElement SearchResults_ReturnFlightsListView;
-	
-	@FindBy(how=How.XPATH,using="(//*[@type='submit' and text()='Book'])[2]")
-	public WebElement SearchResults_BookTicket;
-	
-	
-	
 
+	
 }
