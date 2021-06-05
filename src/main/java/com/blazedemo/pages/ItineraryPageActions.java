98cfa826a0da926ce63435e7d570e26076d35d26
testautomationbyashish <85038148+testautomationbyashish@users.noreply.github.com>
2021-06-05 11:26:55 +0530

    Update ItineraryPageActions.java

--- src/main/java/com/blazedemo/pages/ItineraryPageActions.java
+++ src/main/java/com/blazedemo/pages/ItineraryPageActions.java
@@ -7,40 +7,31 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
-import com.clearTrip.pages.locators.ItineraryPageLocators;
-import com.clearTrip.utils.SeleniumDriver;
-import com.cucumber.listener.Reporter;
 
 import steps.FlightsSearchResults;
 
-public class ItineraryPageActions {
-	public static List<String> onWardFlightDetailsItineraryPage=new ArrayList<String>();
-	public static List<String> returnFlightDetailsItineraryPage=new ArrayList<String>();
-	ItineraryPageLocators itinaryPageloc=null;
+public class CustomerInfoPage {
 	
-	public ItineraryPageActions()
-	{
-		itinaryPageloc=new ItineraryPageLocators();
-		PageFactory.initElements(SeleniumDriver.getDriver(), itinaryPageloc);
-	}
 	
-	public void getOnwardJourneyFlightDetails()
+	
+	
+	public void getCustomerName()
 	{
 		
 	}
 
-	public void getReturnJourneyFlightDetails()
+	public void getCustomerCreditCardDetails()
 	{
 		
 	}
 	
 	
-	public void verifyItineraryOnWardFlightDetails_With_SearchPageOnwardFlightDetails()
+	public void getTotalPrice()
 	{
 		
 	}
 	
-	public void verifyItineraryReturnFlightDetails_With_SearchPageRetunFlightDetails()
+	public void getCustomerAddress()
 	{
 		
 	}
